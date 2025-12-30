86325d26d84dfff1ac158b28c4f9de52b4fa8e44
Renan_Fernandes <orenanfer@gmail.com>
2025-12-30 17:53:43 -0300

    Classic_Cars - Feat: Começando a criar a estrutura inicial do aplicativo

---
+++ experiments/classic_cars/app/app/src/main/java/br/com/renan/classiccars/MainActivity.kt
@@ -0,0 +1,21 @@
+package br.com.renan.classiccars
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import br.com.renan.classiccars.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private var _binding: ActivityMainBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        binding.carsRecyvlerVeiw.layoutManager = LinearLayoutManager(this)
+    }
+}
